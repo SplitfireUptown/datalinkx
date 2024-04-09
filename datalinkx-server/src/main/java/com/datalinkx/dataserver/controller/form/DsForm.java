8e34b71b4dd2f24c704cde841675123693a75fcc
uptown <963842069@qq.com>
2024-04-09 21:16:50 +0800

    沉淀！！🍯🍯

--- datalinkx-server/src/main/java/com/datalinkx/dataserver/controller/form/DsForm.java
+++ datalinkx-server/src/main/java/com/datalinkx/dataserver/controller/form/DsForm.java
@@ -1,7 +1,5 @@
 package com.datalinkx.dataserver.controller.form;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
--- datalinkx-server/src/main/java/com/datalinkx/dataserver/service/impl/JobService.java
+++ datalinkx-server/src/main/java/com/datalinkx/dataserver/service/impl/JobService.java
@@ -8,7 +8,6 @@ import static com.datalinkx.common.utils.JsonUtils.toJson;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
